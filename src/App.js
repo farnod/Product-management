3a3199f3a6afae1dc6c4a1afc85a7a2f4f0dc1a3
mohamad farnoud <farnoodcc@gmail.com>
2023-01-19 00:33:59 +0330

    Create a RTL Theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
+const theme = createTheme({
+	direction: "rtl",
+});
+
+const cacheRTL = createCache({
+	key: "muirtl",
+	stylisPlugins: [prefixer, rtlPlugin],
+});
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>This program is for sales management</h1>
-			</div>
+			<CacheProvider value={cacheRTL}>
+				<ThemeProvider theme={theme}>
+					<HelmetProvider>
+						<Helmet>
+							<title>مدیریت کالاهای ارسالی</title>
+						</Helmet>
+						<div>
+							<h1>مدیریت کالاهای ارسالی</h1>
+						</div>
+					</HelmetProvider>
+				</ThemeProvider>
+			</CacheProvider>
 		);
 	}
 }
